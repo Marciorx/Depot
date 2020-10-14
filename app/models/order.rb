66408b40f05602a351e8735d070c6554e25fecac
Marcio Magalhaes <marcio.mag@outlook.com>
2020-10-14 16:59:24 -0300

    End of Iteration G1

---
+++ app/models/order.rb
@@ -0,0 +1,13 @@
+class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  
+  enum pay_type: {
+    "Check"           => 0,
+    "Credit Card"     => 1,
+    "Purchase order"  => 2
+  }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.key
+
+end
